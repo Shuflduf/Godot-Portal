cba44888741bf411ef7efcf3df95b1910a45f595
Shuflduf <shuflduf@gmail.com>
2025-03-25 19:14:15 -0600

    adding shaders

--- world.gd
+++ world.gd
@@ -8,10 +8,10 @@ extends Node3D
 @onready var cam_two: Camera3D = %CamTwo
 
 func _process(_delta: float) -> void:
-    var m = portal_two.global_transform * (portal_one.global_transform.inverse() * player.cam.global_transform).rotated(Vector3.UP, PI)
+    var m = portal_two.global_transform * portal_one.global_transform.inverse() * player.cam.global_transform
     cam_one.global_transform = m
 
-    var n = portal_one.global_transform * (portal_two.global_transform.inverse() * player.cam.global_transform).rotated(Vector3.UP, PI)
+    var n = portal_one.global_transform * portal_two.global_transform.inverse() * player.cam.global_transform
     cam_two.global_transform = n
 
     DebugDraw3D.draw_camera_frustum(cam_one, Color.ORANGE)
